e33b4fecdab0d9da2b0d99dc280f533365db89fe
gabrixlfernando <biel_nando2012@hotmail.com>
2023-07-20 13:39:33 -0300

    disparo do e-mail em php

--- contato.php
+++ contato.php
@@ -1,3 +1,90 @@
+<?php
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$ok = 0;
+
+
+if (isset($_POST['email'])) {
+
+  //Load Composer's autoloader
+  require './mailer/Exception.php';
+  require './mailer/PHPMailer.php';
+  require './mailer/SMTP.php';
+
+  //Create an instance; passing `true` enables exceptions
+  $mail = new PHPMailer(true);
+
+  try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.hostinger.com';                   //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'cloudwise@smpsistema.com.br';                     //SMTP username
+    $mail->Password   = 'Senac@agencia03';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Receber o e-mail
+    $mail->setFrom('cloudwise@smpsistema.com.br', 'Site AGÊNCIA TIPI');
+    $mail->addAddress('biel_nando2012@hotmail.com');     //Destinatario
+
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    // //Anexo
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Conteudo corpo do e=mail
+
+
+    // dados do e-mail
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel'];
+    $mens = $_POST['mens'];
+
+    $mail->CharSet = 'UTF-8';
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Site Agência Tipi';
+    $mail->Body    = "
+    
+      Nome: $nome <br>
+      E-mail: $email <br>
+      Telefone: $tel <br>
+      Mensagem: $mens 
+    
+    
+    ";
+    $mail->AltBody = "
+    
+    Nome: $nome <br>
+    E-mail: $email <br>
+    Telefone: $tel <br>
+    Mensagem: $mens 
+
+    ";
+
+    $mail->send();
+    $ok = 1;
+    // echo 'Mensagem enviada com SUCESSO!';
+  } catch (Exception $e) {
+    $ok = 2;
+    // echo "ERROOOOO... Tente mais tarde: {$mail->ErrorInfo}";
+  }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
