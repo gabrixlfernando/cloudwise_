c55e2e257b938816bbaaf14d3015734f19cc0cef
gabrixlfernando <biel_nando2012@hotmail.com>
2023-07-29 00:16:26 -0300

    blog

--- conteudo/blog.php
+++ conteudo/blog.php
@@ -7,7 +7,7 @@
         <img src="img/imgBlog.png" alt="">
         <h3>Tecnologia</h3>
         <h4>Equipe <span style="color: #f2f2f2; font-size: 21px">Cloud</span>
-          <span style="color: #ff8900; font-size: 21px">Wise</span> - 02 Jun 2023
+        <span style="color: #ff8900; font-size: 21px">Wise</span> &#x2022; 12 Jun, 2023
         </h4>
         <p>A Importância de Sites Responsivos.</p>
         <a href="siteresponsivo.php">Leia mais<i class="fa-solid fa-arrow-up-right-from-square"></i></a>
@@ -16,7 +16,7 @@
         <img src="img/imgBlog (3).png" alt="">
         <h3>Web Design</h3>
         <h4>Equipe <span style="color: #f2f2f2; font-size: 21px">Cloud</span>
-          <span style="color: #ff8900; font-size: 21px">Wise</span> - 16 Mai 2023
+        <span style="color: #ff8900; font-size: 21px">Wise</span> &#x2022; 16 Mai, 2023
         </h4>
         <p>Técnicas para aumentar a sua criatividade.</p>
         <a href="#">Leia mais<i class="fa-solid fa-arrow-up-right-from-square"></i></a>
--- conteudo/rodape.php
+++ conteudo/rodape.php
@@ -1,62 +1,56 @@
  <footer data-aos="fade-down" data-aos-anchor-placement="top-bottom" data-aos-duration="1500" class="footer">
 
-      <section class="cnt-rodape">
-        <div class="site">
-          <div>
-            <h2>Acompanhe de perto</h2>
-            <hr>
-            <div>
-              <ul>
-                <li>
-                  <a href="https://www.facebook.com" target="_blank"
-                    ><i class="fa-brands fa-facebook"></i
-                  ></a>
-                </li>
-                <li>
-                  <a href="https://www.instagram.com" target="_blank"
-                    ><i class="fa-brands fa-instagram"></i
-                  ></a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com" target="_blank"
-                    ><i class="fa-brands fa-linkedin"></i
-                  ></a>
-                </li>
-              </ul>
-            </div>
-  
-          </div>
-  
-          <div>
-            <h2>Menu</h2>
-            <ul>
-              <li><a href="index.php">Home</a></li>
-              <li><a href="sobre.php">Sobre</a></li>
-              <li><a href="servicos.php">Serviços</a></li>
-              <li><a href="contato.php">Contato</a></li>
-            </ul>
-          </div>
-  
-          <div>
-            <h2>Agência</h2>
-            <a href="contato.php">Av. Marechal Tito, 1500<br>São Paulo-SP</a>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.0254648900814!2d-46.434437023727014!3d-23.495592259180924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce63dda7be6fb9%3A0xa74e7d5a53104311!2sSenac%20S%C3%A3o%20Miguel%20Paulista!5e0!3m2!1spt-BR!2sbr!4v1690573051445!5m2!1spt-BR!2sbr" width="250" height="100" style="border:0px; border-radius: 15px 25px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </div>
-  
-          <div>
-            <h2>Contato</h2>
-            <a href="contato.php">+55 (11) 91234-5678</a>
-            <a href="contato.php">cloudwisedevs@gmail.com</a>
-          </div>
-        </div>
-        
-      </section>
+   <section class="cnt-rodape">
+     <div class="site">
+       <div>
+         <h2>Acompanhe de perto</h2>
+         <hr>
+         <div>
+           <ul>
+             <li>
+               <a href="https://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+             </li>
+             <li>
+               <a href="https://www.instagram.com" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+             </li>
+             <li>
+               <a href="https://www.linkedin.com" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+             </li>
+           </ul>
+         </div>
 
-      <section class="cnt-direitos">
-        <div>
-          <p>2023 Todos os direitos reservados &#169;</p>
+       </div>
 
-          <!-- <div>
+       <div>
+         <h2>Menu</h2>
+         <ul>
+           <li><a href="index.php">Home</a></li>
+           <li><a href="sobre.php">Quem Somos</a></li>
+           <li><a href="servicos.php">Serviços</a></li>
+           <li><a href="contato.php">Contato</a></li>
+         </ul>
+       </div>
+
+       <div>
+         <h2>Agência</h2>
+         <a href="contato.php">Av. Marechal Tito, 1500<br>São Paulo-SP</a>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.0254648900814!2d-46.434437023727014!3d-23.495592259180924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce63dda7be6fb9%3A0xa74e7d5a53104311!2sSenac%20S%C3%A3o%20Miguel%20Paulista!5e0!3m2!1spt-BR!2sbr!4v1690573051445!5m2!1spt-BR!2sbr" width="250" height="100" style="border:0px; border-radius: 15px 25px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+       </div>
+
+       <div>
+         <h2>Contato</h2>
+         <a href="contato.php">+55 (11) 91234-5678</a>
+         <a href="contato.php">cloudwisedevs@gmail.com</a>
+       </div>
+     </div>
+
+   </section>
+
+   <section class="cnt-direitos">
+     <div>
+       <p>2023 Todos os direitos reservados &#169;</p>
+
+       <!-- <div>
             <a href="#">Termos & Condições</a>
             <a href="#">Privacidade</a>
           </div>
@@ -64,10 +58,8 @@
           <div>
             <img src="img/logo (3).png" alt="">
           </div> -->
-        </div>
-        
-        
-      </section>
-    </footer>
+     </div>
 
 
+   </section>
+ </footer>
