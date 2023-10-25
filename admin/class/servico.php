e67ed14101474b61cbee3297f968adf49bdc2e28
CrockCry <pedrovischisoares@gmail.com>
2023-10-24 22:34:12 -0300

    loginAdd

---
+++ admin/class/servico.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('conexao.php');
+
+class ServicoClass{
+    //ATRIBUTOS
+    public $idServico;
+    public $tituloServico;
+    // public $fotoServico;
+    // public $descricaoFotoServico;
+    public $descricaoServico;
+    public $statusServico;
+    // public $urlServico;
+};
